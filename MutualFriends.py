82e07313a8c172fa47c5073f5e1b0e0a27d61eac
Mehrab Azan <77406365+MehrabAzan@users.noreply.github.com>
2026-07-02 19:50:41 -0500

    worked on more problems

---
+++ MutualFriends.py
@@ -0,0 +1,25 @@
+class Villager:
+    def __init__(self, name, species, catchphrase):
+        self.name = name
+        self.species = species
+        self.catchphrase = catchphrase
+        self.friends = []
+
+    def get_mutuals(self, new_contact):
+        selfSet = {friend.name for friend in self.friends}
+        newContactSet = {friend.name for friend in new_contact.friends}
+        return list(selfSet & newContactSet)
+
+bob = Villager("Bob", "Cat", "pthhhpth")
+marshal = Villager("Marshal", "Squirrel", "sulky")
+ankha = Villager("Ankha", "Cat", "me meow")
+fauna = Villager("Fauna", "Deer", "dearie")
+raymond = Villager("Raymond", "Cat", "crisp")
+stitches = Villager("Stitches", "Cub", "stuffin")
+
+bob.friends = [stitches, raymond, fauna]
+marshal.friends = [raymond, ankha, fauna]
+print(bob.get_mutuals(marshal))
+
+ankha.friends = [marshal]
+print(bob.get_mutuals(ankha))
